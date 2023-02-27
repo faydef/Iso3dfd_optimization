b11c0bc1fdf69f3e63f6dbb15c3164fed8beae00
Noé Haddad <noe.haddad@student-cs.fr>
2023-02-27 14:34:04 +0100

    completed values_nblock

--- tree_graph_generation.py
+++ tree_graph_generation.py
@@ -1,9 +1,22 @@
-def values_nblock(n):
+def values_nblock(n_size):
     """return list of integers corresponding to all the possibilities 
     of nk_block corresponding to a certain nk size for the problem"""
-    return 
+    list_nblock = []
+    if n_size>=2:
+        list_nblock.append(2)
+    if n_size>=4:
+        list_nblock.append(4)
+    if n_size>=8:
+        list_nblock.append(8)
+    
+    i=1
+    while n_size>=(16*i):
+        list_nblock.append(16*i)  #add all multiples of 16 smaller than n_size
+        i+=1
+    
+    return list_nblock
 
-def values_nthread_power_of_2(num_threads_max):
+def values_num_thread_power_of_2(num_threads_max):
     """return list of integers corresponding to all the possibilities 
     of num_threads based on the maximum number of threads.
     The list contains the powers of 2 dividing the num_threads_max"""
@@ -15,6 +28,6 @@ def values_nthread_power_of_2(num_threads_max):
     return list_num_thread
 
 
-def tree_generation(compil_flag_list, simd_list, n1, n2, n3, num_threads_max):
+def tree_generation(compil_flag_list, simd_list, n1_size, n2_size, n3_size, num_threads_max):
     """Returns a tree in the form of dictionnaries of dictionnaries of ... with branches for every choice of parameter:
     -Olevel -simd -num_threads -num_threads -n1_block -n2_block -n3_block"""
