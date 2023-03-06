f80caad7eb7962bd93320a90068acc98dd3f0d64
Noé Haddad <noe.haddad@student-cs.fr>
2023-03-06 18:19:42 +0100

    debug ants

--- main.py
+++ main.py
@@ -55,10 +55,14 @@ if type_algo == 1:
     for i in range(nb_iter):
         print("generation number {}".format(i))
         ants_int = exploration(tree_graph, nb_ants)
-        print(list_dict_parameters)
-        print(ants_int)
-        ants = [[list_dict_parameters[k][j] for j in ants_int[k]]
-                for k in range(nb_ants)]
+        ants = []
+        for k in range(nb_ants):
+            ant_int_path = ants_int[k]
+            ant_path = []
+            for j in range(len(ant_int_path)):
+                ant_path.append(list_dict_parameters[j][ant_int_path[j]])
+            ants.append(ant_path)
+
         ants_score = []
         for ant in ants:
             olevel = ant[0]
