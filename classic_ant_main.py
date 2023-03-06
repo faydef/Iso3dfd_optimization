8f6c2c831ebade8ba014cd0e073135b1f1d709d9
oussamakharouiche <oussama.kharouiche@student-cs.fr>
2023-03-06 17:36:47 +0100

    avoid worst path

--- classic_ant_main.py
+++ classic_ant_main.py
@@ -10,6 +10,7 @@ import time
 def ant(nb_ant, nb_iteration, problem, rho, alpha, Q, timeout):
     liste, dico = initiate(problem)
     best = [[],0]
+    worst = []
     for j in range(nb_iteration):
         #initiate the problem
         ants = [[],[],[]]
@@ -22,6 +23,14 @@ def ant(nb_ant, nb_iteration, problem, rho, alpha, Q, timeout):
             choices_5 = choices(liste['liste5'], weights=dico['mat5'][liste['liste4'].index(choices_4)], k=1)[0]
             choices_6 = choices(liste['liste6'], weights=dico['mat6'][liste['liste5'].index(choices_5)], k=1)[0]
             path = [choices_1, choices_2, choices_3, choices_4, choices_5, choices_6]
+            while path in worst:
+                choices_1 = choices(liste['liste1'], weights=dico['mat1'], k=1)[0]
+                choices_2 = choices(liste['liste2'], weights=dico['mat2'][liste['liste1'].index(choices_1)], k=1)[0]
+                choices_3 = choices(liste['liste3'], weights=dico['mat3'][liste['liste2'].index(choices_2)], k=1)[0]
+                choices_4 = choices(liste['liste4'], weights=dico['mat4'][liste['liste3'].index(choices_3)], k=1)[0]
+                choices_5 = choices(liste['liste5'], weights=dico['mat5'][liste['liste4'].index(choices_4)], k=1)[0]
+                choices_6 = choices(liste['liste6'], weights=dico['mat6'][liste['liste5'].index(choices_5)], k=1)[0]
+                path = [choices_1, choices_2, choices_3, choices_4, choices_5, choices_6]
             if path in ants[0]:
                 ants[1][ants[0].index(path)] += 1
             else:
@@ -39,8 +48,9 @@ def ant(nb_ant, nb_iteration, problem, rho, alpha, Q, timeout):
         print(timeout)
         #update the weight
         ants[0], ants[1], ants[2] = map(list, zip(*sorted(zip(ants[0], ants[1], ants[2]),key=itemgetter(2), reverse=True)))
-        #update(routes= zip(ants[0][10], ants[1][10]), liste, dico, rho, alpha, Q)
         routes = [(ants[0][i], ants[1][i]) for i in range(min(10, len(ants[0])))]
+        if len(ants[0]) > 10:
+            worst = [ants[0][i] for i in range(len(ants[0])-1, max(-1, len(ants[0])-5),-1)]
         if ants[2][0] > best[1]:
             best = [ants[0][0], ants[2][0]]
         update(routes, liste, dico, rho, alpha, Q)
@@ -48,11 +58,12 @@ def ant(nb_ant, nb_iteration, problem, rho, alpha, Q, timeout):
 
 if __name__ == '__main__':
     _,nb_ant, nb_iteration, problem_1, problem_2, problem_3, rho, alpha, Q, timeout = sys.argv
-    alpha_list = np.linspace(0.1,1,8)
-    result = []
-    for elem in alpha_list:
-        result.append(ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], float(rho), elem, float(Q), int(timeout)))
-    print(result)
+    # alpha_list = np.linspace(0.1,1,8)
+    # result = []
+    # for elem in alpha_list:
+    #     result.append(ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], float(rho), elem, float(Q), int(timeout)))
+    # print(result)
+    print(ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], float(rho), float(alpha), float(Q), int(timeout)))
     
         
         
