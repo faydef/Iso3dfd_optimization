f7263537d6b26fe3e370e86eecfb7bf6c8024a1b
oussamakharouiche <oussama.kharouiche@student-cs.fr>
2023-03-07 22:31:25 +0100

    worst

--- classic_ant_main.py
+++ classic_ant_main.py
@@ -50,52 +50,37 @@ def ant(nb_ant, nb_iteration, problem, rho, alpha, Q, timeout):
         #update the weight
         ants[0], ants[1], ants[2] = map(list, zip(*sorted(zip(ants[0], ants[1], ants[2]),key=itemgetter(2), reverse=True)))
         routes = [(ants[0][i], ants[1][i]) for i in range(min(10, len(ants[0])))]
-        if len(ants[0]) > 10:
-            worst = [ants[0][i] for i in range(len(ants[0])-1, max(-1, len(ants[0])-5),-1)]
+        if len(ants[0]) > 20:
+            for i in range(len(ants[0])-1, max(-1, len(ants[0])-10),-1):
+                worst.append(ants[0][i]) 
         if ants[2][0] > best[1]:
             best = [ants[0][0], ants[2][0]]
         update(routes, liste, dico, rho, alpha, Q)
     return best
 
 if __name__ == '__main__':
-    _,nb_ant, nb_iteration, problem_1, problem_2, problem_3, no_rho, no_alpha, no_Q, timeout = sys.argv
-    alpha_list = [0.5, 0.75, 1, 1.25, 1.5,1.75]
-    rho_list = [0.2, 0.4, 0.6, 0.8]
-    Q_list = [0.25, 0.5, 0.75, 1]
-    result = [[],[],[]]
-    i = 8
-    filename = 'results.txt'
-    with open(filename, 'w') as f:
-        L = ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], 0.4, 0.25, 1, int(timeout))
-        result[0].append((0.4, 0.25, 1))
-        result[1].append(L[0])
-        result[2].append(L[1])
-        f.write(str([(0.4, 0.25, 1), L[0], L[1]]) + '\n')
-        f.flush()  # flush the buffer to ensure data is written to file
-        i+=1
-        for rho in [0.6,0.8]:
-            for Q in Q_list:
-                print('step '+str(i)+'/112')
-                L = ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], rho, 0.25, Q, int(timeout))
-                result[0].append((rho, 0.25, Q))
-                result[1].append(L[0])
-                result[2].append(L[1])
-                f.write(str([(rho, 0.25, Q), L[0], L[1]]) + '\n')
-                f.flush()
-                i+=1
-        for alpha in alpha_list:
-            for rho in rho_list:
-                for Q in Q_list:
-                    print('step '+str(i)+'/112')
-                    L = ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], rho, alpha, Q, int(timeout))
-                    result[0].append((rho, alpha, Q))
-                    result[1].append(L[0])
-                    result[2].append(L[1])
-                    f.write(str([(rho, alpha, Q), L[0], L[1]]) + '\n')
-                    f.flush()  # flush the buffer to ensure data is written to file
-                    i+=1
-    result[0], result[1], result[2] = map(list, zip(*sorted(zip(result[0], result[1], result[2]),key=itemgetter(2), reverse=True)))
-    print([(result[0][i], result[1][i], result[2][i]) for i in range(10)])
+    _,nb_ant, nb_iteration, problem_1, problem_2, problem_3, rho, alpha, Q, timeout = sys.argv
+    print(ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], float(rho), float(alpha), float(Q), int(timeout)))
+    # alpha_list = [0.25, 0.5, 0.75, 1, 1.25, 1.5,1.75]
+    # rho_list = [0.2, 0.4, 0.6, 0.8]
+    # Q_list = [0.25, 0.5, 0.75, 1]
+    # result = [[],[],[]]
+    # i = 8
+    # filename = 'results.txt'
+    # with open(filename, 'w') as f:
+    #     for alpha in alpha_list:
+    #         for rho in rho_list:
+    #             for Q in Q_list:
+    #                 print('step '+str(i)+'/112')
+    #                 L = ant(int(nb_ant), int(nb_iteration), [int(problem_1), int(problem_2), int(problem_3)], rho, alpha, Q, int(timeout))
+    #                 result[0].append((rho, alpha, Q))
+    #                 result[1].append(L[0])
+    #                 result[2].append(L[1])
+    #                 f.write(str([(rho, alpha, Q), L[0], L[1]]) + '\n')
+    #                 f.flush()  # flush the buffer to ensure data is written to file
+    #                 i+=1
+    # result[0], result[1], result[2] = map(list, zip(*sorted(zip(result[0], result[1], result[2]),key=itemgetter(2), reverse=True)))
+    # print([(result[0][i], result[1][i], result[2][i]) for i in range(10)])
             
         
         
