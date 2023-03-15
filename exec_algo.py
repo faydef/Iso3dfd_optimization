59ff9a4e128156a96767ee0ba947525ac82f9a2f
Noé Haddad <noe.haddad@student-cs.fr>
2023-03-15 17:21:15 +0100

    change KMP_AFFINITY in command

--- exec_algo.py
+++ exec_algo.py
@@ -1,5 +1,7 @@
 from subprocess import STDOUT, check_output
 import re
+
+
 def execute(bash_command, timeout):
     try:
         output = check_output(bash_command, timeout=timeout, shell=True)
@@ -10,15 +12,16 @@ def execute(bash_command, timeout):
 
 #    except subprocess.TimeoutExpired:
     except Exception as e:
-        print("#####algo stopped running: ",e)
+        print("#####algo stopped running: ", e)
         output = -99
     return output
 
+
 def command(options):
-    command = options['filename']+' '+options['size1']+' '+options['size2']+' '+options['size3']+' '+options['num_thread']+' 100'+' '+options['dim1']+' '+options['dim2']+' '+options['dim3']
+    command = 'KMP_AFFINTY=compact'+''+options['filename']+' '+options['size1']+' '+options['size2']+' ' + \
+        options['size3']+' '+options['num_thread']+' 100'+' ' + \
+        options['dim1']+' '+options['dim2']+' '+options['dim3']
     command = command + ' | grep flops'
     return command
 
 #execute('cat test.txt | grep flops', 10)
-
-
