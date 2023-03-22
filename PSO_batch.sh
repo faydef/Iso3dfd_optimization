df1ab0d43e50ed604cf1c68079640506adb38863
faydef <fayssal.defaa@j2reliance.co.uk>
2023-03-22 15:24:36 +0100

    add PSO sh

--- PSO_batch.sh
+++ PSO_batch.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/oneapi/compiler/2022.0.2/linux/compiler/lib/intel64_lin
-if ls | grep "^slurm" >/dev/null; then
-	rm slurm*
-fi
-sbatch -N 1  --qos=16nodespu -p PSO.sh
+
+sbatch -N 1  --qos=16nodespu -p cpu_prod PSO_test.sh
