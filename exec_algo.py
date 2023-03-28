0321a1f541391d49cc76906c3f79d16f43bd533d
Kharouiche Oussama <oussama.kharouiche@student-cs.fr>
2023-03-28 21:44:21 +0200

    latest update

--- exec_algo.py
+++ exec_algo.py
@@ -4,9 +4,8 @@ def execute(bash_command, timeout):
     try:
         output = check_output(bash_command, timeout=timeout, shell=True)
         output = output.decode('UTF-8')
-        output2 = output
         result = re.search('flops:(.*) GFlops', output)
-        time = re.search('time:(.*) sec', output2)
+        time = re.search('time:(.*) sec', output)
         tmp = time.group(1)
         time = float(tmp.strip())
         output = result.group(1)
@@ -21,7 +20,7 @@ def execute(bash_command, timeout):
 
 def command(options):
     command = options['filename']+' '+options['size1']+' '+options['size2']+' '+options['size3']+' '+options['num_thread']+' 100'+' '+options['dim1']+' '+options['dim2']+' '+options['dim3']
-    command = command + ' | grep flops'
+    #command = command + ' | grep flops'
     return command
 
 #execute('cat test.txt | grep flops', 10)
--- main.sh
+++ main.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #SBATCH --time=12:00:00
-#SBATCH -N 4
-#SBATCH -n 128
+#SBATCH -N 16
+#SBATCH -n 512
 #SBATCH -p cpu_prod
 #SBATCH --exclusive
 #SBATCH --qos=16nodespu
-#SBATCH --sockets-per-node=2
-#SBATCH --cores-per-socket=8
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/oneapi/compiler/2022.0.2/linux/compiler/lib/intel64_lin
 
@@ -14,4 +12,4 @@ cd intel_ant_colony
 # Use the script to generate the hostfile
 ./generate_hostfile.sh
 
-mpirun --hostfile hostfile --rank-by node --map-by ppr:1:node:PE=32 python3 parallel_ant_main.py 32 10 128 128 128 0.8 0.25 1 30
+mpirun --hostfile hostfile --rank-by node --map-by ppr:1:node:PE=16 python3 parallel_ant_main.py 100 40 512 512 512 0.8 0.25 0.0001 30
