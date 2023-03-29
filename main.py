20610df462fda913d28c377aa473363b13f96e39
Noé Haddad <noe.haddad@student-cs.fr>
2023-03-29 17:41:40 +0200

    linting

--- main.py
+++ main.py
@@ -153,7 +153,8 @@ def cmdLineParsing():
         sys.exit("Error: maximum nb of iterations must be an integer greater than 0!")
 
     if args.m not in ("ACO", "Tree_ACO", "PSO", "firework"):
-        sys.exit("Error: local method must be in: [ACO, Tree_ACO, PSO,firework]!")
+        sys.exit(
+            "Error: local method must be in: [ACO, Tree_ACO, PSO,firework]!")
 
     return (
         args.i,
@@ -223,15 +224,21 @@ if not len(sys.argv) > 1:
         )
 
     if type_algo == 0 or type_algo == 1:
-        nb_iter = ask_for("number of iterations (10 by default) : ", DefaultIterMax)
+        nb_iter = ask_for(
+            "number of iterations (10 by default) : ", DefaultIterMax)
         nb_ants = ask_for(
             "number of ants per iteration (100 by default) : ", Defaultnumberpopulation
         )
-        evaporation_rate = input("evaporation_rate (0.8 by default) : ") or Defaultalpha
-        n1_size = ask_for("size of the problem x (default size 256) : ", Defaultn1)
-        n2_size = ask_for("size of the problem y (default size 256) : ", Defaultn2)
-        n3_size = ask_for("size of the problem z (default size 256) : ", Defaultn3)
-        n_threads_max = ask_for("maximum number of threads (32 by default): ", 32)
+        evaporation_rate = input(
+            "evaporation_rate (0.8 by default) : ") or Defaultalpha
+        n1_size = ask_for(
+            "size of the problem x (default size 256) : ", Defaultn1)
+        n2_size = ask_for(
+            "size of the problem y (default size 256) : ", Defaultn2)
+        n3_size = ask_for(
+            "size of the problem z (default size 256) : ", Defaultn3)
+        n_threads_max = ask_for(
+            "maximum number of threads (32 by default): ", 32)
 
     if type_algo == 0:
         print("executing classic ant colony optimization...")
@@ -258,10 +265,14 @@ if not len(sys.argv) > 1:
         c1 = input("particle speed (1.5 by default) : ") or 1.5
         c2 = input("group_speed (1.5 by default) : ") or 1.5
         w = input("inertia (0.5 by default) : ") or Defaultw
-        problem_1 = ask_for("size of the problem x (default size 256) : ", Defaultn1)
-        problem_2 = ask_for("size of the problem y (default size 256) : ", Defaultn2)
-        problem_3 = ask_for("size of the problem z (default size 256) : ", Defaultn3)
-        n_threads_max = ask_for("maximum number of threads (32 by default) : ", 32)
+        problem_1 = ask_for(
+            "size of the problem x (default size 256) : ", Defaultn1)
+        problem_2 = ask_for(
+            "size of the problem y (default size 256) : ", Defaultn2)
+        problem_3 = ask_for(
+            "size of the problem z (default size 256) : ", Defaultn3)
+        n_threads_max = ask_for(
+            "maximum number of threads (32 by default) : ", 32)
         timeout = ask_for("duration before timeout (30 by default) : ", 30)
         print("executing particle swarm optimization...")
 
@@ -303,7 +314,8 @@ if len(sys.argv) > 1:
             compil_flag_list, simd_list, n_threads_max, n1, n2, n3, alpha, i, n, timeout
         )
     if method == "PSO":
-        bounds = [(0, 2), (0, 2), (1, n_threads_max), (16, n1), (1, n2), (1, n3)]
+        bounds = [(0, 2), (0, 2), (1, n_threads_max),
+                  (16, n1), (1, n2), (1, n3)]
         optimizer = ParticleSwarmOptimization(
             objective_function, bounds, n, i, c1, c2, w, [n1, n2, n3], timeout,
         )
